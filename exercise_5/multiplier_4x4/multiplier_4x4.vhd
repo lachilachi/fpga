c23b2f6031779b295ab8779967a5653842de2f0c
lachilachi <33571030+lachilachi@users.noreply.github.com>
2021-12-09 16:47:04 +0100

    Add files via upload

--- exercise_5/multiplier_4x4/multiplier_4x4.vhd
+++ exercise_5/multiplier_4x4/multiplier_4x4.vhd
@@ -15,213 +15,210 @@ entity multiplier_4x4 is
 end multiplier_4x4;
 
 architecture rtl of multiplier_4x4 is
+
+-- signal declaration
+signal stemp : std_logic_vector (19 downto 0) := (others => '0');
+signal ctemp : std_logic_vector (19 downto 0) := (others => '0');
+signal atemp : std_logic_vector (19 downto 0) := (others => '0');
+signal btemp : std_logic_vector (19 downto 0) := (others => '0');
+signal ptemp : std_logic_vector (19 downto 0) := (others => '0');
+
+
 -- component declaration
-component mu is
+--component comb_logic is
+--  port (s_in : in std_logic := '0';
+--        b_in : in std_logic := '0';
+--        a_in : in std_logic := '0';
+--        c_in : in std_logic := '0';
+
+--        s_out : out std_logic := '0';
+--        b_out : out std_logic := '0';
+--        c_out : out std_logic := '0';
+--        a_out : out std_logic := '0');
+--end component comb_logic;
+
+
   
-    port (s_in : in std_logic := '0';
-        b_in : in std_logic := '0';
-        a_in : in std_logic := '0';
-        c_in : in std_logic := '0';
-
-        s_out : out std_logic := '0';
-        b_out : out std_logic := '0';
-        c_out : out std_logic := '0';
-        a_out : out std_logic := '0');
-		
-end component mu;
-
---用到的变量
-variable btemp: std_logic_vector (19 downto 0);
-variable stemp: std_logic_vector (19 downto 0);
-variable atemp: std_logic_vector (19 downto 0);
-variable ctemp: std_logic_vector (19 downto 0); 
-variable ptemp: std_logic_vector (7 downto 0);
-
-
+  
 begin
 
---组件间缓存
-  io_register : process (clk,reset)
+
+
+  io_register : process (clk, reset)
   begin  -- process mult
     if rising_edge(clk) then            -- rising clock edge
       if reset = '1' then               -- synchronous reset (active high)
-        btemp := (others => '0');
-		stemp := (others => '0');
-		atemp := (others => '0');
-		ctemp := (others => '0');
-		ptemp := (others => '0');
-		o_p <= (others => '0');
-		
+
+        atemp(0) <= '0';
+        atemp(4) <= '0';
+        atemp(8) <= '0';
+        atemp(12) <= '0';
+        btemp(0) <= '0';
+        btemp(1) <= '0';
+        btemp(2) <= '0';
+        btemp(3) <= '0';
+        ptemp <= (others => '0');
+        
       else
-		btemp := (0 => i_b(0), 1 => i_b(1), 2 => i_b(2), 3 => i_b(3));
-		atemp(0) := i_a(0); 
-        atemp(5) := i_a(1); 
-		atemp(10) := i_a(2); 
-		atemp(15) := i_a(3); 
-		
-		ptemp(0) := stemp(4); 
-        ptemp(1) := stemp(8); 
-		ptemp(2) := stemp(12); 
-		ptemp(3) := stemp(16); 
-		ptemp(4) := stemp(17); 
-		ptemp(5) := stemp(18); 
-		ptemp(6) := stemp(19); 
-		ptemp(7) := ctemp(19); 
-		
-		o_p <= ptemp;
+        
+        atemp(0) <=i_a(0) ;
+        atemp(4) <=i_a(1) ;
+        atemp(8) <=i_a(2) ;
+        atemp(12) <=i_a(3) ;
+        btemp(0) <= i_b(0);
+        btemp(1) <= i_b(1);
+        btemp(2) <= i_b(2);
+        btemp(3) <= i_b(3);
+
+        o_p(7) <= ctemp(19);
+        o_p(6) <= stemp(19);
+        o_p(5) <= stemp(18);
+        o_p(4) <= stemp(17);
+        o_p(3) <= stemp(16);
+        o_p(2) <= stemp(12);
+        o_p(1) <= stemp(8);
+        o_p(0) <= stemp(4);
+
+        
+        
       end if;
     end if;
   end process io_register;
 
+
+
+ 
+ 
+ 
 ----------------------------------------------------------------------------------------------------
 -- Generate statements
--- nested loop with j = row, i = column
+--nested loop with j = row, i = column
 --------------------------------------------------------------------------------------------------------------
 
-row : for j in 0 to 3 generate
-	row_begin: if (j = 0) generate
-	
-		column : for i in 0 to 3 generate			
-			column_begin: if (i = 0) generate
-				mu_begin: mu
-				port map(
-					s_in => stemp(4*j+i),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
+ -- row : for j in 0 to 3 generate
+    column : for i in 0 to 15 generate
+      
+      -- first row 
+      
+     -- first_row: if (j=0) generate
+      begin
+        
+      column_begin_1: if (i=0) generate
+      MU1: entity work.mu port map (s_in => stemp(i),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => ctemp(i+1),
+          a_out => atemp(i+1));
+      end generate;
 
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_middle: if (i > 0) and (i < 3) generate
-				mu_middle: mu
-				port map(
-					s_in => stemp(4*j+i),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
+      column_mittel_1: if (i=1) and (i=2) generate
+      MU2: entity work.mu
+        port map (
+          s_in => stemp(i),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => ctemp(i+1),
+          a_out => atemp(i+1));
+      end generate;
+      
+      column_end_1: if(i=3) generate
+      MU3: entity work.mu
+        port map (
+          s_in => stemp(i),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => ptemp(i),
+          a_out => open);
+      end generate;
+      
+      column_end_2: if(i=7) and (i=11) and (i=15) generate
+      MU4: entity work.mu
+        port map (
+          s_in => ptemp(i-4),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => ptemp(i),
+          a_out => open);
+      end generate;
+      
+      column_mittel_2: if (i=5) and (i=6) and (i=9)and (i=10)and (i=13)and (i=14)generate
+      MU5: entity work.mu
+        port map (
+          s_in => stemp(i+1),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => ctemp(i+1),
+          a_out => atemp(i+1));
+      end generate;
+      
+       column_begin_2: if (i=4) and (i=8)and (i=12)generate
+      MU6: entity work.mu port map (s_in => stemp(i+1),
+          b_in => btemp(i),
+          a_in => atemp(i),
+          c_in => ctemp(i),
+          s_out => stemp(i+4),
+          b_out => btemp(i+4),
+          c_out => stemp(i+1),
+          a_out => atemp(i+1));
+      end generate;
+  --  end generate first_row;
+      
+      
+     --j_1: if (j>0 and j<3) generate
+    
+     -- column : for i in 0 to 4 generate
+      --column_begin: if (i=0) generate
+     -- mu_begin: entity work.mu
+       -- port map (
+         -- s_in => stemp((j-1)*4+i+1),
+     --     b_in => i_b(i),
+       --   a_in => i_a(j),
+      --    c_in => '0',
+          
+      --    s_out => stemp(i+j*4),
+      --    b_out => btemp(i),
+      --    c_out => ctemp(i),
+       --   a_out => atemp(j));
+      --end generate;
 
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_end: if (i = 3) generate
-				mu_end: mu
-				port map(
-					s_in => stemp(4*j+i),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
+   --   column_mittel: if (i>0) and (i<4) generate
+   --   mu_mittel: entity work.mu
+    --    port map (
+     --     s_in => stemp((j-1)*4+i+1),
+      --    b_in => btemp(i),
+     --     a_in => atemp(j),
+     --     c_in => ctemp(j*4+i-1),
+          
+      --    s_out => stemp(i+j),
+      --    b_out => btemp(i),
+      --    c_out => ctemp(j*4+i),
+       --   a_out => atemp(j));
+     -- end generate;
 
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => open,
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-		end generate column;
-	end generate
-	
-	row_middle: if (j > 0) and (j < 3) generate
-	
-		column : for i in 0 to 3 generate			
-			column_begin: if (i = 0) generate
-				mu_begin: mu
-				port map(
-					s_in => stemp(4*j+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
+  --  end generate;
 
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_middle: if (i > 0) and (i < 3) generate
-				mu_middle: mu
-				port map(
-					s_in => stemp(4*j+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
+      
+    end generate column;
+--  end generate row;
 
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_end: if (i = 3) generate
-				mu_end: mu
-				port map(
-					s_in => ctemp(5*(j-1)+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
-
-					s_out => stemp(4*(j+1)+i),
-					b_out => btemp(4*(j+1)+i),
-					a_out => open,
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-		end generate column;
-	end generate
-	
-	row_end: if (j = 3) generate
-	
-		column : for i in 0 to 3 generate			
-			column_begin: if (i = 0) generate
-				port map(
-					s_in => stemp(4*j+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
-
-					s_out => stemp(4*(j+1)+i),
-					b_out => open,
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_middle: if (i > 0) and (i < 3) generate
-				mu_middle: mu
-				port map(
-					s_in => stemp(4*j+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
-
-					s_out => stemp(4*(j+1)+i),
-					b_out => open,
-					a_out => atemp(5*j+i+1),
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-			column_end: if (i = 3) generate
-				mu_end: mu
-				port map(
-					s_in => stemp(5*(j-1)+i+1),
-					b_in => btemp(4*j+i),
-					a_in => atemp(5*j+i),
-					c_in => ctemp(5*j+i),
-
-					s_out => stemp(4*(j+1)+i),
-					b_out => open,
-					a_out => open,
-					c_out => ctemp(5*j+i+1));
-				end generate;
-				
-		end generate column;
-	end generate
-	
-end generate row;
 
 end rtl;
