38ba766c7cba329a78164967699ce57c2aefe171
z.zheng <704026725@qq.com>
2021-12-15 21:30:08 +0100

    ex6

--- exercise_6/sync_pulse_generator/sync_pulse_generator.vhd
+++ exercise_6/sync_pulse_generator/sync_pulse_generator.vhd
@@ -29,10 +29,10 @@ entity sync_pulse_generator is
    --G_V_SYNC_LENGTH    : vertical sync pulse length in pixels
    --G_V_SYNC_ACTIVE    : vertical sync pulse polarity (1 = pos, 0 = neg)
    generic (G_H_PIXEL_NUMBER : integer := 800;
-            G_H_RESOLUTION : integer := 640;
-            G_H_FRONT_PORCH : integer := 8;
-            G_H_BACK_PORCH : integer := 48;
-            G_H_SYNC_LENGTH : integer := 96;
+            G_H_RESOLUTION : integer := 640; --4
+            G_H_FRONT_PORCH : integer := 8;  --1
+            G_H_BACK_PORCH : integer := 48;  --3
+            G_H_SYNC_LENGTH : integer := 96; --2
             G_H_SYNC_ACTIVE : std_logic := '0';
              
             G_V_PIXEL_NUMBER : integer := 525;
@@ -54,6 +54,192 @@ end sync_pulse_generator;
 
 architecture rtl of sync_pulse_generator is
 
-             
+begin
+
+column: process(clk, reset)
+variable i:integer := 1;
+variable j:integer := 1;
+begin
+	if reset = '1' then
+		o_h_sync <= G_H_SYNC_ACTIVE;
+		o_v_sync <= G_V_SYNC_ACTIVE;
+		o_in_active_region <= '0';
+	
+	elsif clk'event and clk='1' then
+		if (j>0 and j<G_V_FRONT_PORCH) then
+			if (i>0 and i<G_H_FRONT_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+		
+			elsif (i>=G_H_FRONT_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH) then
+				o_h_sync <= G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION and i<=G_H_PIXEL_NUMBER) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				if i>G_H_PIXEL_NUMBER then 
+					i:=1;
+					j := j+1;
+				end if;
+			end if;
+			o_v_sync <= not G_V_SYNC_ACTIVE;
+			o_in_active_region <= '0';
+			
+		
+		elsif (j>=G_V_FRONT_PORCH and j<G_V_FRONT_PORCH+G_V_SYNC_LENGTH) then
+			if (i>0 and i<G_H_FRONT_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+		
+			elsif (i>=G_H_FRONT_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH) then
+				o_h_sync <= G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION and i<=G_H_PIXEL_NUMBER) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				if i>G_H_PIXEL_NUMBER then 
+					i:=1;
+					j := j+1;
+				end if;
+			end if;
+			o_v_sync <= G_V_SYNC_ACTIVE;
+			o_in_active_region <= '0';
+			
+			
+		elsif (j>=G_V_FRONT_PORCH+G_V_SYNC_LENGTH and j<G_V_FRONT_PORCH+G_V_SYNC_LENGTH+G_V_BACK_PORCH) then
+			if (i>=0 and i<G_H_FRONT_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+		
+			elsif (i>=G_H_FRONT_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH) then
+				o_h_sync <= G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION and i<=G_H_PIXEL_NUMBER) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				if i>G_H_PIXEL_NUMBER then 
+					i:=1;
+					j := j+1;
+				end if;
+			end if;
+			o_v_sync <= not G_V_SYNC_ACTIVE;
+			o_in_active_region <= '0';
+			
+		
+		elsif (j>=G_V_FRONT_PORCH+G_V_SYNC_LENGTH+G_V_BACK_PORCH and j<G_V_FRONT_PORCH+G_V_SYNC_LENGTH+G_V_BACK_PORCH+G_V_RESOLUTION) then
+			if (i>=0 and i<G_H_FRONT_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				o_in_active_region <= '0';
+				i := i+1;
+		
+			elsif (i>=G_H_FRONT_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH) then
+				o_h_sync <= G_H_SYNC_ACTIVE;
+				o_in_active_region <= '0';
+				i := i+1;
+				
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				o_in_active_region <= '0';
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				o_in_active_region <= '1';
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION and i<=G_H_PIXEL_NUMBER) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				o_in_active_region <= '0';
+				i := i+1;
+				if i>G_H_PIXEL_NUMBER then 
+					i:=1;
+					j := j+1;
+				end if;
+			end if;
+			o_v_sync <= not G_V_SYNC_ACTIVE;
+			
+			
+		
+		elsif (j>G_V_FRONT_PORCH+G_V_SYNC_LENGTH+G_V_BACK_PORCH+G_V_RESOLUTION and j<=G_V_PIXEL_NUMBER) then
+			if (i>0 and i<G_H_FRONT_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+		
+			elsif (i>=G_H_FRONT_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH) then
+				o_h_sync <= G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH and i<G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+			
+			elsif (i>=G_H_FRONT_PORCH+G_H_SYNC_LENGTH+G_H_BACK_PORCH+G_H_RESOLUTION and i<=G_H_PIXEL_NUMBER) then
+				o_h_sync <= not G_H_SYNC_ACTIVE;
+				
+				i := i+1;
+				if i>G_H_PIXEL_NUMBER then 
+					i:=1;
+					j := j+1;
+				end if;
+			end if;
+			o_v_sync <= not G_V_SYNC_ACTIVE;
+			o_in_active_region <= '0';
+			
+			if j>G_V_PIXEL_NUMBER then j:=1;
+			end if;
+		end if;
+	end if;
+end process;
+		
 end rtl;
 
